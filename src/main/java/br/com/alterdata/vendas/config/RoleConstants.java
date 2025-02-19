d31457f0ab5964a6422a8a1dffcc5ec30b324c87
igoralves367 <igor.alves367@gmail.com>
2025-02-19 16:54:47 -0300

    Implementa parte dos Roles de acesso e rotas de permissão

---
+++ src/main/java/br/com/alterdata/vendas/config/RoleConstants.java
@@ -0,0 +1,11 @@
+package br.com.alterdata.vendas.config;
+
+public class RoleConstants {
+	private RoleConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+    
+}
