4ec590f776c05a646a9d74f4681f141a59881014
igoralves367 <igor.alves367@gmail.com>
2025-02-19 16:53:11 -0300

    Implementa Usario parte do service e repository

---
+++ src/main/java/br/com/alterdata/vendas/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package br.com.alterdata.vendas.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alterdata.vendas.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	Optional<User> findByLogin(String login);
+
+}
