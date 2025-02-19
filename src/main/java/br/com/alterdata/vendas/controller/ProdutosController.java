e3ff816e0c449fb40e55b37e0bc324f7d2fd463c
igoralves367 <igor.alves367@gmail.com>
2025-02-19 13:23:06 -0300

    Inclusão de Logs no Produto Controller

--- src/main/java/br/com/alterdata/vendas/controller/ProdutosController.java
+++ src/main/java/br/com/alterdata/vendas/controller/ProdutosController.java
@@ -17,10 +17,12 @@ import br.com.alterdata.vendas.dto.ProdutoDTO;
 import br.com.alterdata.vendas.model.Produto;
 import br.com.alterdata.vendas.service.ProdutoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("produtos")
 @RequiredArgsConstructor
+@Log4j2
 public class ProdutosController {
 
     private final ProdutoService produtoService;
@@ -32,32 +34,46 @@ public class ProdutosController {
 
 	@PostMapping
 	public ResponseEntity<ProdutoDTO> criaProduto(@RequestBody ProdutoDTO produto) {
+		log.info("[start] ProdutosController - criaProduto");
 		ProdutoDTO criaProduto = produtoService.criaProduto(produto);
+		log.info("[finish] ProdutosController - criaProduto");
 		return new ResponseEntity<>(criaProduto, HttpStatus.CREATED);
 	}
 
 	@GetMapping("/buscarProduto/{id}")
 	public ResponseEntity<ProdutoDTO> buscarProdutoPorId(@PathVariable Long id) {
+		log.info("[start] ProdutosController - buscarProdutoPorId");
+		log.info("[idProduto] {}", id);
 		ProdutoDTO produto = produtoService.buscarProdutoPorId(id);
+		log.info("[finish] ProdutosController - buscarProdutoPorId");
 		return new ResponseEntity<>(produto, HttpStatus.OK);
 
 	}
 
     @PutMapping("/atualizaProduto/{id}")
     public ResponseEntity<ProdutoDTO> atualizarProduto(@PathVariable Long id, @RequestBody ProdutoDTO atualizaProduto) {
+    	log.info("[start] ProdutosController - atualizarProduto");
+		log.info("[idProduto] {}", id);
         ProdutoDTO alterarProduto = produtoService.alterarProduto(id, atualizaProduto);
+        log.info("[finish] ProdutosController - atualizarProduto");
         return new ResponseEntity<>(alterarProduto, HttpStatus.OK);
     }
     
     @DeleteMapping("/deletarProduto/{id}")
     public ResponseEntity<Void> excluirProduto(@PathVariable Long id) {
+    	log.info("[start] ProdutosController - excluirProduto");
+		log.info("[idProduto] {}", id);
         produtoService.deletarProduto(id);
+        log.info("[finish] ProdutosController - excluirProduto");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
     
     @GetMapping("/produtos/categoria/{NomeCategoria}")
     public ResponseEntity<List<ProdutoDTO>> encontrarProdutosPorCategoria(@PathVariable String NomeCategoria) {
+    	log.info("[start] ProdutosController - encontrarProdutosPorCategoria");
+		log.info("[NomeCategoria] {}", NomeCategoria);
         List<ProdutoDTO> produtos = produtoService.buscarProdutosPorCategoria(NomeCategoria);
+        log.info("[finish] ProdutosController - encontrarProdutosPorCategoria");
         return new ResponseEntity<>(produtos, HttpStatus.OK);
     }
 }
