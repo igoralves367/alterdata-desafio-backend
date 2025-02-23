876423acbd4c69b05650c7d1fcf23f686142713e
igoralves367 <igor.alves367@gmail.com>
2025-02-23 07:44:16 -0300

    Organiza DataHelp e sql de produtos

--- src/test/resources/seeds/produtos.sql
+++ src/test/resources/seeds/produtos.sql
@@ -1,23 +1,30 @@
-INSERT INTO produtos (id, nome, descricao, referencia, valor_unitario) VALUES
+INSERT INTO categorias (id, nome) VALUES
+    (1, 'Ferramentas'),
+    (2, 'Jardinagem'),
+    (3, 'Equipamentos');
+
+INSERT INTO produtos (id, nome, descricao, referencia, valor_unitario, categoria_id) VALUES
     (
         1,
         'Motosserra Semi Profissional Gasolina 50,2Cc Tcs53X-20',
-        'As Motosserras Toyama Foram Desenvolvidas E Fabricadas Especialmente Para O Usuário Que Busca Qualidade De Equipamento Com Baixo Custo De Aquisição. Para Torná-La Leve, Durável E Confiável Utilizamos Em Sua Produção, Liga De Magnésio De Alta Resistência, Equipadas Com As Correntes Oregon.',
+        'As Motosserras Toyama Foram Desenvolvidas E Fabricadas Especialmente Para O Usuário Que Busca Qualidade De Equipamento Com Baixo Custo De Aquisição.',
         'TCS53H18',
-        399.90
+        399.90,
+        1 
     ),
     (
         2,
         'Tesoura Para Poda + Serrote de Poda',
-        'Tesourão para Poda Linha Bronze, para podar frutíferas, flores e plantas ornamentais, galhos e ramos de árvores onde não é possível alcançar com as tesouras normais. Para ajudar no seu trabalho, esse jogo contém um serrote para poda com 2 lâminas.',
+        'Tesourão para Poda Linha Bronze, para podar frutíferas, flores e plantas ornamentais.',
         'KIT140',
-        163.54
+        163.54,
+        2 
     ),
     (
         3,
         'Kit para Jardinagem Horta Tramontina 04 Peças com Luva',
-        'Kit para Jardinagem Horta Tramontina 04 Peças com Luva. Kit completo com a qualidade Tramontina para pequenas hortas, jardins, hobby, floricultura.',
+        'Kit completo para pequenas hortas, jardins e floricultura.',
         '55374237',
-        57.34
-    )
-;
+        57.34,
+        3 
+    );
