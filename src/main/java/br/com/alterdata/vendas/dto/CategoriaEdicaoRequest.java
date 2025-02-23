6c57d7c1650e06ee737c8a8f1f2ef98fe72c52ec
igoralves367 <igor.alves367@gmail.com>
2025-02-23 08:02:06 -0300

    Atualização na estrutura e lógica de Categoria

---
+++ src/main/java/br/com/alterdata/vendas/dto/CategoriaEdicaoRequest.java
@@ -0,0 +1,12 @@
+package br.com.alterdata.vendas.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CategoriaEdicaoRequest {
+    private String nome;
+}
--- src/main/java/br/com/alterdata/vendas/model/Categoria.java
+++ src/main/java/br/com/alterdata/vendas/model/Categoria.java
@@ -7,15 +7,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import br.com.alterdata.vendas.dto.CategoriaEdicaoRequest;
+import br.com.alterdata.vendas.dto.CategoriaRequest;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "categorias")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
 public class Categoria {
 
 	@Id
@@ -24,5 +29,12 @@ public class Categoria {
 
 	@Column(nullable = false)
 	private String nome;
+	
+	public Categoria(CategoriaRequest categoriaRequest) {
+		this.nome = categoriaRequest.getNome();
+	}
 
+	public void edita(CategoriaEdicaoRequest atualizaCategoria) {
+		this.nome = atualizaCategoria.getNome();		
+	}
 }
